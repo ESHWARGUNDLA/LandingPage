d0177998b46dcc45528c6cc6353ad3015e22d93f
ESHWARGUNDLA <eshwargundla2001@gmail.com>
2024-02-19 23:19:20 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+    <h1 className='bg-blue-400 p-2 m-2'>hello world</h1>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Github/Github.jsx
@@ -0,0 +1,26 @@
+import React, { useEffect } from 'react'
+// import { useState } from 'react'
+import { useLoaderData } from 'react-router-dom'
+function Github() {
+const data = useLoaderData()
+    // const [data,setData] = useState([])
+    // useEffect(() =>{
+    // fetch('https://api.github.com/users/ESHWARGUNDLA')
+    // .then((response) =>response.json())
+    // .then((data) => {
+    //     setData(data)
+    // })
+    // },[])
+  return (
+    <div className='text-center m-4 bg-gray-600 text-white 
+    p-4 text-3xl'>Github follwers:{data.followers}
+    <img src={data.avatar_url} alt="avatar" />
+    </div>
+  )
+}
+
+export default Github
+export const githubInfoLoader = async () =>{
+    const response = await fetch('https://api.github.com/users/ESHWARGUNDLA')
+    return response.json()
+}
